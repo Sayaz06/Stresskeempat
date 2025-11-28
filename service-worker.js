40863befaa768cce9012ab5f8f953e9ba740c91b
Sayaz06 <adha86614@gmail.com>
2025-11-29 07:19:11 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = "stresskeempat-cache-v1";
+const CACHE_NAME = "stresskeempat-cache-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -39,3 +39,4 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
+
